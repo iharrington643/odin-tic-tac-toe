9f447a712d83dfccfa18a4ad28e96301da45426d
Isabella Harrington <isabellaharrington@gmail.com>
2025-08-19 21:35:54 -0400

    Created gameboard array inside of Gameboard factory function for storing grid cell values
    Created functions for editing and copying gameboard array content

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
+const gridItems = document.getElementsByClassName('grid-item');
+
 const Gameboard = (function() {
     const gameboard = new Array(9).fill(null);
+
+    function updateBoard(index, marker) {
+        if (gameboard[index] === '') {
+            gameboard[index] = marker;
+        }
+    };
+
+    function resetBoard() {
+        gameboard.fill(null);
+    }
+
+    function getBoard() {
+        const gameboardCopy = Array.from(gameboard);
+        return gameboardCopy;
+    }
+
 })();
+
+const GameController = (function() {
+
+})();
+
+function Player(name, marker) {
+    return { name, marker };
+}
 
